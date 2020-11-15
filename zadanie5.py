8685b670c8f79f0ff937694d603b5a29df152609
Tomasz <40639418+TiPiKej@users.noreply.github.com>
2020-11-15 21:14:49 +0100

    Update zadanie5.py

--- zadanie5.py
+++ zadanie5.py
@@ -1,6 +1,10 @@
+from random import randint
+
 imiona = ['Damian', 'Ola', 'Barbara', 'Robert', 'Zygmunt', 'Ewa']
 
 pierwsze_litery = [imie[0] for imie in imiona]
+losowe_liczby = [[randint(1, 10) for j in range(4)] for i in range(5)]
 
 print(imiona)
 print(pierwsze_litery)
+print(losowe_liczby)
