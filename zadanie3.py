fdb7d0037364f8f20fb8b2ad788c1b20caab0115
Tomasz <40639418+TiPiKej@users.noreply.github.com>
2020-12-15 20:52:24 +0100

    Update zadanie3.py
    Dodałem klauzurę else

--- zadanie3.py
+++ zadanie3.py
@@ -7,5 +7,7 @@ for i in range(liczba_prob):
     if klucz == in_klucz:
         sukces = True
         break
+    else:
+        print("Slowo {} nie pasuje do klucza!".format(in_klucz))
 
 print("Gratulacje!" if sukces else "Przegrales!")
