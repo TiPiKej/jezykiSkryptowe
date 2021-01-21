1fe4efa972d4253d1f3bd88a46c15839bd6e9de5
Tomasz <40639418+TiPiKej@users.noreply.github.com>
2021-01-21 13:47:43 +0100

    .

---
+++ zadanie11.py
@@ -0,0 +1,14 @@
+import json
+from openpyxl import Workbook
+
+wb = Workbook()
+ws = wb.active
+try:
+    with open("zadanie11.json", "r") as json_file:
+        dane = json.load(json_file)
+        for komorka in dane:
+            ws.cell(row=komorka['row'], column=komorka['column']).value = komorka['value']
+            print(komorka)
+        wb.save("zdanie11.xlsx")
+except IOError:
+    print("Blad IO")
